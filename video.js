28e646029da918b4e03032dafa9e216c77943440
iAmLemon <45285698+iAmLemon@users.noreply.github.com>
2020-05-10 19:06:44 +0800

    Create video.js

---
+++ video.js
@@ -0,0 +1,10 @@
+new DPlayer({
+    container: document.getElementById('dplayer'),
+    screenshot: true,
+    video: {
+        url: 'http://gzc-download.azcc.com.cn/api/videoplay/28cec8f6077212b0bbfd20ed19b274a32aed7bbd.mp4',
+        pic: 'http://5b0988e595225.cdn.sohucs.com/images/20191018/ed1d38a5594142bbb938bba7d34a3c31.jpeg',
+        
+    },
+    logo:'http://i0.hdslb.com/bfs/active/520da726ae420bde8ab1d908c98ae4fd3a4ac449.png'
+});
