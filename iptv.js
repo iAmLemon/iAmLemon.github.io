dc243cbe33dfdeaa7b1ada364c756b7fe67a446c
iAmLemon <45285698+iAmLemon@users.noreply.github.com>
2020-05-11 16:51:25 +0800

    Create iptv.js

---
+++ iptv.js
@@ -0,0 +1,25 @@
+new DPlayer({
+    container: document.getElementById('dplayer'),
+    screenshot: true,
+    video: {
+       
+        quality: [
+            {
+                name: 'CCTV1',
+                url: 'http://live-temp-litchi-hls-yf.jstv.com/live/zhibo-cctv1gq/online.m3u8',
+                type: 'hls',
+            },
+            {
+                name: '凤凰中文',
+                url: 'http://116.199.5.52:8114/index.m3u8?Fsv_chan_hls_se_idx=190&FvSeid=1&Fsv_ctype=LIVES&Fsv_otype=1&Provider_id=&Pcontent_id=.m3u8',
+                type: 'hls',
+            },
+        ],
+
+        defaultQuality: 0,
+        
+        
+        
+    },
+ 
+});
