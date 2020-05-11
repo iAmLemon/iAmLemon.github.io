82dc5f0d0f6a6f5014ca5bf62f7fc6e74ce5cff6
iAmLemon <45285698+iAmLemon@users.noreply.github.com>
2020-05-11 08:13:51 +0800

    Update video.js

--- video.js
+++ video.js
@@ -2,7 +2,7 @@ new DPlayer({
     container: document.getElementById('dplayer'),
     screenshot: true,
     video: {
-        url: 'http://gzc-download.azcc.com.cn/api/videoplay/28cec8f6077212b0bbfd20ed19b274a32aed7bbd.mp4',
+        url: 'https://gss3.baidu.com/6LZ0ej3k1Qd3ote6lo7D0j9wehsv/tieba-smallvideo/607272_4bff3a968176dd461ca93a0031a79658.mp4',
         pic: 'http://5b0988e595225.cdn.sohucs.com/images/20191018/ed1d38a5594142bbb938bba7d34a3c31.jpeg',
         
     },
