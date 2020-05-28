23da45ee15c7b4d65e98948f804298134f8ee4a2
iAmLemon <45285698+iAmLemon@users.noreply.github.com>
2020-05-28 12:03:55 +0800

    Update video.js

--- video.js
+++ video.js
@@ -2,7 +2,7 @@ new DPlayer({
     container: document.getElementById('dplayer'),
     screenshot: true,
     video: {
-        url: 'https://gss3.baidu.com/6LZ0ej3k1Qd3ote6lo7D0j9wehsv/tieba-smallvideo/607272_4bff3a968176dd461ca93a0031a79658.mp4',
+        url: 'https://gss3.baidu.com/6LZ0ej3k1Qd3ote6lo7D0j9wehsv/tieba-smallvideo/19649098_5f9d840a145a8b536675633378311dac.mp4',
         pic: 'https://5b0988e595225.cdn.sohucs.com/images/20191018/ed1d38a5594142bbb938bba7d34a3c31.jpeg',
         
     },
