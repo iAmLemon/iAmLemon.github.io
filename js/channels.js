d5cbe86b0a5ee90fc7f9adfd4d03b5738c29f682
iAmLemon <45285698+iAmLemon@users.noreply.github.com>
2020-06-07 20:08:57 +0800

    Create channels.js

---
+++ js/channels.js
@@ -0,0 +1,35 @@
+var channels = [{
+	"firstword": "H动漫",
+	"list": [{
+		"title": "炎の孕ませもっと!発育っ!身体测定2-THE.ANIMATION（中）",
+		"url": "https://bangumi.ml/m3u8/48655.m3u8"
+	}, {
+		"title": "フリフレ.THE.ANIMATION～.制服堕天使の课外授业～",
+		"url": "https://bangumi.ml/m3u8/7831.m3u8"
+	}, {
+		"title": "Love.Selection～THE.ANIMATION～Select.2“Favorite.Menu",
+		"url": "https://bangumi.ml/m3u8/5258.m3u8"
+	}, {
+		"title": "妹ぱらだいす！1～お兄ちゃん、わたしとしようよっ～",
+		"url": "https://bangumi.ml/m3u8/5266.m3u8"
+	}, {
+		"title": "Swing.Out.Sisters-完全版",
+		"url": "https://bangumi.ml/m3u8/12764.m3u8"
+	}, {
+		"title": "片田舎に嫁いできた○シア娘とHしまくるお话.若奥様は幽霊が苦手の巻（Ⅰ）",
+		"url": "https://bangumi.ml/m3u8/9998.m3u8"
+	},{
+		"title": "片田舎に嫁いできた○シア娘とHしまくるお话.若奥様はお祭りに行きたいの巻（Ⅱ）",
+		"url": "https://bangumi.ml/m3u8/8823.m3u8"
+	}, {
+		"title": "片田舎に嫁いできた○シア娘とHしまくるお话.若奥様がメイド服でご奉仕の巻（Ⅳ）",
+		"url": "https://bangumi.ml/m3u8/7015.m3u8"
+	}, {
+		"title": "ピスはめ!5",
+		"url": "https://bangumi.ml/m3u8/6818.m3u8"
+	},
+	{
+		"title": "相思相爱ノート.THE.ANIMATION.Breast.2“おっぱい、おっぱい、おっぱい”",
+		"url": "https://bangumi.ml/m3u8/36413.m3u8"
+	},]
+}];
