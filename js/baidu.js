a9896d61ee147031ecdea1fcada2448d4da4d299
iAmLemon <45285698+iAmLemon@users.noreply.github.com>
2020-06-13 11:31:43 +0800

    Create baidu.js

---
+++ js/baidu.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a62c71815aaf88710ac01b2a7a4b1fb4";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
