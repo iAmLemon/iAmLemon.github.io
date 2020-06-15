0625a980873ca6b7d4ceaa7a72bf0ddd5061bc14
iAmLemon <45285698+iAmLemon@users.noreply.github.com>
2020-06-15 19:39:13 +0800

    Update channels.js

--- js/channels.js
+++ js/channels.js
@@ -1,73 +1,71 @@
-var channels = [{
-	"firstword": "H动漫",
-	"list": [{
-		"title": "炎の孕ませもっと!発育っ!身体测定2-THE.ANIMATION（中）",
-		"url": "https://bangumi.ml/m3u8/48655.m3u8"
-	}, {
-		"title": "フリフレ.THE.ANIMATION～.制服堕天使の课外授业～",
-		"url": "https://bangumi.ml/m3u8/7831.m3u8"
-	}, {
-		"title": "Love.Selection～THE.ANIMATION～Select.2“Favorite.Menu",
-		"url": "https://bangumi.ml/m3u8/5258.m3u8"
-	}, {
-		"title": "妹ぱらだいす！1～お兄ちゃん、わたしとしようよっ～",
-		"url": "https://bangumi.ml/m3u8/5266.m3u8"
-	}, {
-		"title": "Swing.Out.Sisters-完全版",
-		"url": "https://bangumi.ml/m3u8/12764.m3u8"
-	}, {
-		"title": "片田舎に嫁いできた○シア娘とHしまくるお话.若奥様は幽霊が苦手の巻（Ⅰ）",
-		"url": "https://bangumi.ml/m3u8/9998.m3u8"
+varchannels=[{
+	"firstword":"H动漫",
+	"list":[{
+		"title":"炎の孕ませもっと!発育っ!身体测定2-THE.ANIMATION（中）",
+		"url":"https://bangumi.ml/m3u8/48655.m3u8"
 	},{
-		"title": "片田舎に嫁いできた○シア娘とHしまくるお话.若奥様はお祭りに行きたいの巻（Ⅱ）",
-		"url": "https://bangumi.ml/m3u8/8823.m3u8"
-	}, {
-		"title": "片田舎に嫁いできた○シア娘とHしまくるお话.若奥様がメイド服でご奉仕の巻（Ⅳ）",
-		"url": "https://bangumi.ml/m3u8/7015.m3u8"
-	}, {
-		"title": "ピスはめ!5",
-		"url": "https://bangumi.ml/m3u8/6818.m3u8"
+		"title":"フリフレ.THE.ANIMATION～.制服堕天使の课外授业～",
+		"url":"https://bangumi.ml/m3u8/7831.m3u8"
+	},{
+		"title":"Love.Selection～THE.ANIMATION～Select.2“Favorite.Menu",
+		"url":"https://bangumi.ml/m3u8/5258.m3u8"
+	},{
+		"title":"妹ぱらだいす！1～お兄ちゃん、わたしとしようよっ～",
+		"url":"https://bangumi.ml/m3u8/5266.m3u8"
+	},{
+		"title":"Swing.Out.Sisters-完全版",
+		"url":"https://bangumi.ml/m3u8/12764.m3u8"
+	},{
+		"title":"片田舎に嫁いできた○シア娘とHしまくるお话.若奥様は幽霊が苦手の巻（Ⅰ）",
+		"url":"https://bangumi.ml/m3u8/9998.m3u8"
+	},{
+		"title":"片田舎に嫁いできた○シア娘とHしまくるお话.若奥様はお祭りに行きたいの巻（Ⅱ）",
+		"url":"https://bangumi.ml/m3u8/8823.m3u8"
+	},{
+		"title":"片田舎に嫁いできた○シア娘とHしまくるお话.若奥様がメイド服でご奉仕の巻（Ⅳ）",
+		"url":"https://bangumi.ml/m3u8/7015.m3u8"
+	},{
+		"title":"ピスはめ!5",
+		"url":"https://bangumi.ml/m3u8/6818.m3u8"
 	},
 	{
-		"title": "相思相爱ノート.THE.ANIMATION.Breast.2“おっぱい、おっぱい、おっぱい”",
-		"url": "https://bangumi.ml/m3u8/36413.m3u8"
+		"title":"相思相爱ノート.THE.ANIMATION.Breast.2“おっぱい、おっぱい、おっぱい”",
+		"url":"https://bangumi.ml/m3u8/36413.m3u8"
 	},
-                {
-		"title": "Newmanoid CAM～キャム=キャスティン～",
-		"url": "https://bangumi.ml/m3u8/1787.m3u8"
+{
+		"title":"NewmanoidCAM～キャム=キャスティン～",
+		"url":"https://bangumi.ml/m3u8/1787.m3u8"
 	},
-                {
-		"title": "放课后の优等生 1［笹森トモエ］",
-		"url": "https://bangumi.ml/m3u8/3396.m3u8"
+{
+		"title":"放课后の优等生1［笹森トモエ］",
+		"url":"https://bangumi.ml/m3u8/3396.m3u8"
 	},
-                {
-		"title": "少交女 “绚音と柚とお兄ちゃん”",
-		"url": "https://bangumi.ml/m3u8/3411.m3u8"
+{
+		"title":"少交女“绚音と柚とお兄ちゃん”",
+		"url":"https://bangumi.ml/m3u8/3411.m3u8"
 	},
-                {
-		"title": "漫吃ハプニング ラッキースケベ、危うし！！社长令嬢が差し出すのは捜查令状！？ 编",
-		"url": "https://bangumi.ml/m3u8/3867.m3u8"
+{
+		"title":"漫吃ハプニングラッキースケベ、危うし！！社长令嬢が差し出すのは捜查令状！？编",
+		"url":"https://bangumi.ml/m3u8/3867.m3u8"
 	},
-                {
-		"title": "一求乳魂 前编",
-		"url": "https://bangumi.ml/m3u8/10979.m3u8"
+{
+		"title":"一求乳魂前编",
+		"url":"https://bangumi.ml/m3u8/10979.m3u8"
 	},
-                {
-		"title": "真希#3 真希ちゃんとなう。 ～真希＆由希のド变态共同‘性’活◆_～",
-		"url": "https://bangumi.ml/m3u8/16034.m3u8"
+{
+		"title":"真希#3真希ちゃんとなう。～真希＆由希のド变态共同‘性’活◆_～",
+		"url":"https://bangumi.ml/m3u8/16034.m3u8"
 	},
-                {
-		"title": "天然恋色アルコール 后编 ～ヒミツノハナゾノ＆ Honey Snow～",
-		"url": "https://bangumi.ml/m3u8/35992.m3u8"
+{
+		"title":"天然恋色アルコール后编～ヒミツノハナゾノ＆HoneySnow～",
+		"url":"https://bangumi.ml/m3u8/35992.m3u8"
 	},
-                {
-		"title": "新体操（真）",
-		"url": "https://bangumi.ml/m3u8/36405.m3u8"
+{
+		"title":"新体操（真）",
+		"url":"https://bangumi.ml/m3u8/36405.m3u8"
 	},
-                {
-		"title": "JKとエロ议员センセイ“ドジっ娘メガネ秘书・初音～拗ねて抓ってハメスクワット",
-		"url": "https://bangumi.ml/m3u8/39834.m3u8"
-	},
-              
-]
+{
+		"title":"JKとエロ议员センセイ“ドジっ娘メガネ秘书・初音～拗ねて抓ってハメスクワット",
+		"url":"https://bangumi.ml/m3u8/39834.m3u8"
+	}]
 }];
