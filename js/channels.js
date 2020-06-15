54eb6152e24a4a45b54bebecf9662edec67faa16
iAmLemon <45285698+iAmLemon@users.noreply.github.com>
2020-06-15 19:19:51 +0800

    Update channels.js

--- js/channels.js
+++ js/channels.js
@@ -31,8 +31,43 @@ var channels = [{
 	{
 		"title": "相思相爱ノート.THE.ANIMATION.Breast.2“おっぱい、おっぱい、おっぱい”",
 		"url": "https://bangumi.ml/m3u8/36413.m3u8"
-	},{
-		"title": "初犬2 ストレンジ カインド オブ ウーマン#2",
-		"url": "https://bangumi.ml/m3u8/12773.m3u8"
-	},]
+	},
+                {
+		"title": "Newmanoid CAM～キャム=キャスティン～",
+		"url": "https://bangumi.ml/m3u8/1787.m3u8"
+	},
+                {
+		"title": "放课后の优等生 1［笹森トモエ］",
+		"url": "https://bangumi.ml/m3u8/3396.m3u8"
+	},
+                {
+		"title": "少交女 “绚音と柚とお兄ちゃん”",
+		"url": "https://bangumi.ml/m3u8/3411.m3u8"
+	},
+                {
+		"title": "漫吃ハプニング ラッキースケベ、危うし！！社长令嬢が差し出すのは捜查令状！？ 编",
+		"url": "https://bangumi.ml/m3u8/3867.m3u8"
+	},
+                {
+		"title": "一求乳魂 前编",
+		"url": "https://bangumi.ml/m3u8/10979.m3u8"
+	},
+                {
+		"title": "真希#3 真希ちゃんとなう。 ～真希＆由希のド变态共同‘性’活◆_～",
+		"url": "https://bangumi.ml/m3u8/16034.m3u8"
+	},
+                {
+		"title": "天然恋色アルコール 后编 ～ヒミツノハナゾノ＆ Honey Snow～",
+		"url": "https://bangumi.ml/m3u8/35992.m3u8"
+	},
+                {
+		"title": "新体操（真）",
+		"url": "https://bangumi.ml/m3u8/36405.m3u8"
+	},
+                {
+		"title": "JKとエロ议员センセイ“ドジっ娘メガネ秘书・初音～拗ねて抓ってハメスクワット",
+		"url": "https://bangumi.ml/m3u8/39834.m3u8"
+	},
+              
+]
 }];
